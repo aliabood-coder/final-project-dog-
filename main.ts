dae9c9b107017c2a5a41ea8159d36335df6b654f
aliabood-coder <83079653+aliabood-coder@users.noreply.github.com>
2021-06-14 14:36:16 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let projectile: Sprite = null
 let creates_and_gates = [img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -54,3 +55,9 @@ let dog = sprites.create(img`
 dog.setStayInScreen(true)
 dog.bottom = 120
 controller.moveSprite(dog, 100, 100)
+info.setLife(3)
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(creates_and_gates[randint(0, creates_and_gates.length - 1)], 0, 75)
+    projectile.setKind(SpriteKind.Enemy)
+    projectile.x = randint(10, 150)
+})
