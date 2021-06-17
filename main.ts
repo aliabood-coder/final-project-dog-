4e6c3a427794188737ca8419aac9c4ec68e6f7fd
aliabood-coder <83079653+aliabood-coder@users.noreply.github.com>
2021-06-17 15:26:41 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,53 +1,51 @@
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    biscuit.destroy(effects.disintegrate, 500)
+    projectile2.destroy(effects.hearts, 500)
     info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     scene.cameraShake(4, 500)
-    creates_and_gates.destroy(effects.smiles, 500)
+    projectile.destroy(effects.smiles, 500)
     dog.startEffect(effects.fire, 200)
     info.changeLifeBy(-1)
 })
 let projectile: Sprite = null
+let projectile2: Sprite = null
 let dog: Sprite = null
-let biscuit: Sprite = null
-let creates_and_gates: Image = null
-creates_and_gates = img`
-    2 2 . . . . . . . . . . . . . 2 
-    . 2 2 . . . . . . . . . . . 2 2 
-    . . 2 . . . . . . . . . . 2 2 . 
-    . . . 2 . . . . . . . . 2 . . . 
-    . . . . 2 . . . . . . 2 2 . . . 
-    . . . . . 2 2 . . . 2 2 . . . . 
-    . . . . . . 2 2 . 2 2 . . . . . 
-    . . . . . . . 2 2 2 . . . . . . 
-    . . . . . . . 2 2 2 . . . . . . 
-    . . . . . . 2 2 . 2 . . . . . . 
-    . . . . . 2 2 . . . 2 2 . . . . 
-    . . . . 2 2 . . . . . 2 . . . . 
-    . . . 2 2 . . . . . . 2 2 . . . 
-    . . 2 2 . . . . . . . . 2 . . . 
-    2 2 2 . . . . . . . . . . 2 . . 
-    2 . . . . . . . . . . . . 2 2 2 
-    `
-biscuit = sprites.create(img`
+let creates_and_gates = [img`
+    . . . . . . . . . . . . . . 2 2 
+    . 2 . . . . . . . . . . . 2 2 . 
+    . . 2 . . . . . . . . 2 2 . . . 
+    . . . 2 . . . . . . 2 2 . . . . 
+    . . . . 2 . . . 2 2 2 . . . . . 
+    . . . . . 2 . 2 2 . . . . . . . 
+    . . . . . . 2 2 . . . . . . . . 
+    . . . . . . 2 2 . . . . . . . . 
+    . . . . . 2 . 2 2 . . . . . . . 
+    . . . . 2 . . . 2 . . . . . . . 
+    . . . . 2 . . . 2 2 . . . . . . 
+    . . . 2 . . . . . 2 . . . . . . 
+    . . . 2 . . . . . 2 . . . . . . 
+    . . 2 . . . . . . . 2 . . . . . 
+    . 2 . . . . . . . . 2 . . . . . 
+    . 2 . . . . . . . . . 2 . . . . 
+    `, img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . 2 2 . . . . . . . . . 
-    . . . . . 2 2 2 . . . . . . . . 
-    . . . . . 2 e 2 2 . . . . . . . 
-    . . . . . 2 e e 2 2 . . . . . . 
-    . . . . . 2 e e e 2 . . . . . . 
-    . . . . . 2 e e e 2 2 . . . . . 
-    . . . . . 2 e e e e 2 2 . . . . 
-    . . . . . 2 e e e e e 2 . . . . 
-    . . . . . 2 e e e e e 2 2 . . . 
-    . . . . . 2 e e e 2 2 2 2 2 . . 
-    . . . . . 2 e e 2 2 . . . . . . 
-    . . . . . 2 2 2 2 . . . . . . . 
-    . . . . . . . 2 . . . . . . . . 
-    `, SpriteKind.Food)
+    . . . 3 . . . . . . . . . . . . 
+    . . . 3 3 3 3 3 3 . . . . . . . 
+    . . . 3 . . . . 3 3 3 3 3 3 3 . 
+    . . . 3 . . . . . . . . . . 3 . 
+    . . . 3 3 3 3 3 . . . . . 3 3 . 
+    . . . . . . . 3 3 3 3 3 3 3 . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `]
 dog = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -71,7 +69,27 @@ dog.bottom = 120
 controller.moveSprite(dog, 100, 100)
 info.setLife(3)
 game.onUpdateInterval(500, function () {
-    projectile = sprites.createProjectileFromSide(creates_and_gates, 0, 75)
+    projectile = sprites.createProjectileFromSide(creates_and_gates[randint(0, creates_and_gates.length - 1)], 0, 75)
+    projectile2 = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . 8 . . . 
+        . . . . . . . . . . . . 8 . . . 
+        . . . . . . . . . . 8 8 . . . . 
+        . . . . . . . . 8 8 8 . . . . . 
+        . . . . . . . . . . 8 8 . . . . 
+        . . . . . . . . . 8 . 8 8 8 . . 
+        . . . . . . . . . 8 . 8 . . 8 8 
+        . . . . . . . . . 8 . 8 . . . 8 
+        . . . . . . . . 8 . . 8 . 8 8 8 
+        . . . . . . . . 8 . . 8 . 8 . 8 
+        . . . . . . . . 8 . . 8 . 8 . 8 
+        . . . . . . . . 8 . . . 8 8 . 8 
+        . . . . . . . . 8 . . . . 8 8 . 
+        . . . . . . . . 8 . . 8 8 . . . 
+        . . . . . . . . 8 8 8 . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, 0, 75)
     projectile.setKind(SpriteKind.Enemy)
     projectile.x = randint(10, 150)
+    projectile2.setKind(SpriteKind.Food)
+    projectile2.x = randint(10, 150)
 })
